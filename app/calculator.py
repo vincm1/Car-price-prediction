e8222a15a70268165c531cb6035ec28740596554
vincm1 <vincent.michler94@gmail.com>
2023-09-26 17:53:40 +0200

    added price calc page

--- app/calculator.py
+++ app/calculator.py
@@ -1,10 +1,54 @@
 """Car price predictor Streamlit App"""
-import pickle
 import streamlit as st
 import xgboost as xgb
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error, mean_absolute_error
 
-xgb_model = pickle.load(open('xgboost_regressor.pkl','rb'))
+df = pd.read_csv("cleaned_df.csv", delimiter=",")
+
+bst = xgb.Booster()
+bst.load_model('xgboost_best_regressor.json')
+
+def predict_car_price(input_data):
+    bst.predict()
+    return None
+
+### Form options ###
+marken = df.make.unique()
+models = df.model.unique()
+fuel_types = df.fuel.unique()
+offer_types = df.offerType.unique()
+gear_types = df.gear.unique()
+
+### Page rendering ###
 
 def render_calculator_page():
     """ Func for rendering the calcualtor page"""
-    st.title("Preisrechner")
+    col1, col2 = st.columns(2, gap="large")
+    with col1:
+        st.title(":car: :money_mouth_face: Gebrauchtwagenpreisrechner")
+    
+    with col2: 
+        st.image('kaefer.jpg')
+    
+    st.divider()
+    st.subheader("Gib uns nähere Infos zum Fahrzeug")
+    col1, col2, col3 = st.columns(3, gap="large")
+    with col1:
+        marke = st.selectbox(label="Marke des Fahrzeugs", options=marken)
+        model = st.selectbox(label="Model", options=models)
+
+    with col2:
+        mileage = st.number_input("KM Laufleistung:", min_value=0)
+        hp = st.slider(label="PS", min_value=1, max_value=1000, step=1)
+        
+    with col3:
+        year = st.number_input(label="Jahr")
+        fuel_type = st.selectbox(label="Antrieb", options=fuel_types)
+        gear_type = st.selectbox(label="Schaltung", options=gear_types)
+        offer_type = st.selectbox(label="Angebot", options=offer_types)
+        
+    car_predict = {"mileage":mileage, "marke":marke, "model":model, "hp":hp, "year":year, "fuel_type":fuel_type, "gear_type":gear_type, "offer_type":offer_type}
+    st.text(f"{car_predict}")
+    st.button("Preis berechnen")
--- app/main.py
+++ app/main.py
@@ -3,10 +3,12 @@ import streamlit as st
 import pandas as pd
 from streamlit_option_menu import option_menu
 from calculator import render_calculator_page
+import matplotlib.pyplot as plt
 
 #### Dataset ####
 
 df = pd.read_csv("cleaned_df.csv", delimiter=",")
+df = df[["mileage","make", "model","fuel","gear","offerType","price","hp", "year"]].copy()
 
 #### Page setup ####
 
@@ -31,21 +33,70 @@ def render_homepage():
         st.title(":oncoming_automobile: Dein unabhängiger Gebrauchtwagenpreisrechner!", )
         st.subheader("Lass dir den Preis deines Gebrauchtwagen berechnen.")
         st.text(f"Anhand von {len(df)} Fahrzeugen den besten Preis für deinen Gebrauchten finden.")
-    
+        st.text("Datenquelle: https://www.kaggle.com/datasets/ander289386/cars-germany")
     with col2: 
         st.image('cars.jpg')
+    st.divider()
+    #compute top analytics
+    mean_price = float(df.price.mean())
+    mean_mileage = float(df.mileage.mean())
+    marke = tuple(pd.Series(df.make.value_counts().head(1)))
+    model = tuple(pd.Series(df.model.value_counts().head(1)))
+    total1,total2,total3,total4=st.columns(4,gap='large')
+    with total1:
+        st.info('Ø Preis EUR',icon="📌")
+        st.metric(label="Preis", label_visibility="hidden", value=f"{mean_price:,.0f}".replace(',', '.'))
+
+    with total2:
+        st.info('Ø KM-Laufleistung',icon="📌")
+        st.metric(label="Laufleistung", label_visibility="hidden", value=f"{mean_mileage:,.0f}".replace(',', '.'))
+
+    with total3:
+        st.info('Beliebteste Marke',icon="📌")
+        st.metric(label="# Stück in DS",value=f"VW ({marke[0]})")
+
+    with total4:
+        st.info('Beliebtestes model',icon="📌")
+        st.metric(label="# Stück in DS",value=f"Golf ({model[0]})")
     
     st.divider()
+        
+    st.subheader(f"Wie sieht Gebrauchtwagenmarkt aus zwischen {df.year.min()} und {df.year.max()} ?")
     
-    container = st.container()
-    container.write("This is inside the container")
-    st.write("This is outside the container")
+    chart1, chart2, chart3 = st.columns(3, gap="large")
 
-    # Now insert some more in the container
-    container.write("This is inside too")
+    with chart1:
+        st.subheader("Verteilung der Marke:")
+        marke_counts = df.make.value_counts()
+        st.bar_chart(data=marke_counts)
+        
+    with chart2:
+        st.subheader("Verteilung der PS:")
+        # Create a new figure
+        fig, ax = plt.subplots()
+        # Plot the histogram on the figure's axis
+        df.hp.hist(bins=20, ax=ax)
+        # Display the figure in Streamlit
+        st.pyplot(fig)
+        
+    with chart3:
+        st.subheader("Verteilung der PS:")
+        # Create a new figure
+        # Create a new figure and axis
+        fig, ax = plt.subplots()
 
-    # Showcasing dataset #
+        # Plot the scatter plot on the figure's axis
+        ax.scatter(df['mileage'], df['price'], alpha=0.5)
 
+        # Customize the plot
+        ax.set_title("Scatter plot between mileage and price")
+        ax.set_xlabel("Mileage")
+        ax.set_ylabel("Price")
+
+        # Display the figure in Streamlit
+        st.pyplot(fig)
+        
+        
 #### NAVIGATION ####
 # Create a sidebar to navigate between the pages
 #st.sidebar.title("Navigation")
